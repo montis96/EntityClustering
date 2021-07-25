88ff4d9baae98e83fe5c8fd0f20232f65af52fa1
Simone Monti <sgmonti@gmail.com>
2021-07-25 16:10:09 +0200

    aggiunti indici e testo

--- Packages/CoNLLReader.py
+++ Packages/CoNLLReader.py
@@ -1,29 +1,54 @@
+import pandas as pd
 import re
 from pathlib import Path
+from nltk import WhitespaceTokenizer
 
 """
-Parsing del file CONLL
+Parsing del file Ayda-yago CONLL
 """
 
 
-def read_aida_yago_conll(path):
-    file_path = Path(path)
-    raw_text = file_path.read_text(encoding='utf-8')
-
-
-
-
-
-    raw_docs = re.split(r'\n\t?\n', raw_text)
-    token_docs = []
-    tag_docs = []
-    for doc in raw_docs:
-        tokens = []
-        tags = []
-        for line in doc.split('\n'):
-            token, _, tag = line.split('\t')
-            tokens.append(token.lower())
-            tags.append(tag)
-        token_docs.append(tokens)
-        tag_docs.append(tags)
-    return token_docs, tag_docs
+def read_aida_yago_conll(raw_path):
+    path = Path(raw_path)
+    raw_text = path.read_text(encoding='utf-8').strip()
+    raw_line = re.split(r'\n', raw_text)
+    tokens = []
+    tags = []
+    mention = []
+    entities = []
+    wikidatas = []
+    numeric_codes = []
+    alpha_codes = []
+    for raw_word in raw_line:
+        word = raw_word.split("\t")
+        if len(word[0]) > 0 and "DOCSTART" not in word[0]:
+            tokens.append(word[0])
+            try:
+                tags.append(word[1])
+            except:
+                tags.append(None)
+            try:
+                mention.append(word[2])
+            except:
+                mention.append(None)
+            try:
+                entities.append(word[3])
+            except:
+                entities.append(None)
+            try:
+                wikidatas.append(word[4])
+            except:
+                wikidatas.append(None)
+            try:
+                numeric_codes.append(word[5])
+            except:
+                numeric_codes.append(None)
+            try:
+                alpha_codes.append(word[6])
+            except:
+                alpha_codes.append(None)
+    text = " ".join(tokens)
+    indexes = list(WhitespaceTokenizer().span_tokenize(text))
+    return text, pd.DataFrame(list(zip(tokens, indexes, tags, mention, entities, wikidatas, numeric_codes, alpha_codes)),
+                        columns=["tokens", "indexes", "tags", "mention", "entities", "wikidatas", "numeric_codes",
+                                 "alpha_codes"])  # %%
