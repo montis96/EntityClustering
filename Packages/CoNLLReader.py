23fefa4596df42c4293c2a80fc19d286a51c90bf
Simone Monti <sgmonti@gmail.com>
2021-07-28 16:39:19 +0200

    aggiunto documento in dataframe

--- Packages/CoNLLReader.py
+++ Packages/CoNLLReader.py
@@ -22,62 +22,75 @@ def read_aida_yago_conll(raw_path):
     alpha_codes = []
     counter = 0
     indexes = []
+    documents = []
+    document = ""
     for index, raw_word in enumerate(raw_line):
         word = raw_word.split("\t")
-        if len(word[0]) > 0 and "DOCSTART" not in word[0]:
-            # ho riunito i token che sono stati assegnati alla stessa menzione
-            try:
-                if word[1] == "B":
-                    tokens.append(word[2])
-            except:
-                tokens.append(word[0])
-            try:
-                if word[1] == "B" and word[4]:
-                    tags.append(word[1])
-            except:
-                tags.append('')
-            try:
-                if word[1] == "B":
-                    mentions.append(word[2])
-            except:
-                mentions.append('')
-            try:
-                if word[1] == "B":
-                    entities.append(word[3])
-            except:
-                entities.append('')
-            try:
-                if word[1] == "B":
-                    wikidatas.append(word[4])
-            except:
-                wikidatas.append('')
-            try:
-                if word[1] == "B":
-                    numeric_codes.append(word[5])
-            except:
-                numeric_codes.append('')
-            try:
-                if word[1] == "B":
-                    alpha_codes.append(word[6])
-            except:
-                alpha_codes.append('')
-            try:
-                if word[1] == "B":
+        if "DOCSTART" not in word[0]:
+            if len(word[0]) > 0:
+                # ho riunito i token che sono stati assegnati alla stessa menzione
+                documents.append(document)
+
+                try:
+                    if word[1] == "B":
+                        tokens.append(word[2])
+                except:
+                    tokens.append(word[0])
+                try:
+                    if word[1] == "B" and word[4]:
+                        tags.append(word[1])
+                except:
+                    tags.append('')
+                try:
+                    if word[1] == "B":
+                        mentions.append(word[2])
+                except:
+                    mentions.append('')
+                try:
+                    if word[1] == "B":
+                        entities.append(word[3])
+                except:
+                    entities.append('')
+                try:
+                    if word[1] == "B":
+                        wikidatas.append(word[4])
+                except:
+                    wikidatas.append('')
+                try:
+                    if word[1] == "B":
+                        numeric_codes.append(word[5])
+                except:
+                    numeric_codes.append('')
+                try:
+                    if word[1] == "B":
+                        alpha_codes.append(word[6])
+                except:
+                    alpha_codes.append('')
+                try:
+                    if word[1] == "B":
+                        indexes.append((counter, counter + len(tokens[-1])))
+                except:
                     indexes.append((counter, counter + len(tokens[-1])))
-            except:
-                indexes.append((counter, counter + len(tokens[-1])))
-            try:
-                if word[1] == "B":
+                try:
+                    if word[1] == "B":
+                        counter = counter + len(tokens[-1]) + 1
+                except:
                     counter = counter + len(tokens[-1]) + 1
-            except:
-                counter = counter + len(tokens[-1]) + 1
+        else:
+            document = int(word[0].split()[1][1:].replace("testa", "").replace("testb", ""))
     for i in range(len(tags)):
         if entities[i] == '--NME--':
             entities[i] = ''
             tags[i] = ''
             mentions[i] = ''
-    text = " ".join(tokens)
-    dataframe = pd.DataFrame(list(zip(tokens, indexes, tags, mentions, entities, wikidatas, numeric_codes, alpha_codes)),
-                             columns=["tokens", "indexes", "tags", "mentions", "entities", "wikidatas", "numeric_codes",
-                                      "alpha_codes"])
-    return text, dataframe
+    dataframe = pd.DataFrame(
+        list(zip(documents, tokens, indexes, tags, mentions, entities, wikidatas, numeric_codes, alpha_codes)),
+        columns=["documents", "tokens", "indexes", "tags", "mentions", "entities", "wikidatas",
+                 "numeric_codes", "alpha_codes"])
+    # text = text_reconstruction(dataframe)
+    return 'text', dataframe
+
+
+# def text_reconstruction(dataframe):
+
+
