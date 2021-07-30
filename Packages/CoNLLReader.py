2fa58f51b94984e8ea79dc64ec1094ebdb6a3a0c
Simone Monti <sgmonti@gmail.com>
2021-07-30 17:48:16 +0200

    aggiunto metodo per filtrare le entità in base al numero, iniziata implementazione del cluster sul context based on BOW

--- Packages/CoNLLReader.py
+++ Packages/CoNLLReader.py
@@ -3,6 +3,7 @@ import re
 import codecs
 from pathlib import Path
 from nltk import WhitespaceTokenizer
+import numpy as np
 
 """
 Parsing del file Ayda-yago CONLL
@@ -29,13 +30,13 @@ def read_aida_yago_conll(raw_path):
         if "DOCSTART" not in word[0]:
             if len(word[0]) > 0:
                 # ho riunito i token che sono stati assegnati alla stessa menzione
-                documents.append(document)
-
                 try:
                     if word[1] == "B":
                         tokens.append(word[2])
+                        documents.append(document)
                 except:
                     tokens.append(word[0])
+                    documents.append(document)
                 try:
                     if word[1] == "B" and word[4]:
                         tags.append(word[1])
@@ -77,7 +78,9 @@ def read_aida_yago_conll(raw_path):
                 except:
                     counter = counter + len(tokens[-1]) + 1
         else:
-            document = int(word[0].split()[1][1:].replace("testa", "").replace("testb", ""))
+            document = int(word[0].split()[1][1:].replace("testa", "").replace("testb", "")) - 1
+            counter = 0
+
     for i in range(len(tags)):
         if entities[i] == '--NME--':
             entities[i] = ''
@@ -87,10 +90,32 @@ def read_aida_yago_conll(raw_path):
         list(zip(documents, tokens, indexes, tags, mentions, entities, wikidatas, numeric_codes, alpha_codes)),
         columns=["documents", "tokens", "indexes", "tags", "mentions", "entities", "wikidatas",
                  "numeric_codes", "alpha_codes"])
-    # text = text_reconstruction(dataframe)
-    return 'text', dataframe
+    texts = text_reconstruction(dataframe)
+    return texts, dataframe
 
 
-# def text_reconstruction(dataframe):
+def text_reconstruction(dataframe):
+    texts = []
+    for doc in np.unique(dataframe['documents'].values):
+        texts.append(" ".join(dataframe[dataframe['documents'] == doc]['tokens'].values))
+    return texts
 
+
+def filter_data(dataframe, min_el):
+    new_df = dataframe.copy()
+    entities = new_df['entities']
+    not_allowed = []
+    from collections import Counter
+    ent_dict = Counter(entities)
+    for key in ent_dict:
+        if ent_dict[key] < min_el:
+            not_allowed.append(key)
+    new_entities = []
+    for x in new_df['entities']:
+        if x in not_allowed:
+            new_entities.append('')
+        else:
+            new_entities.append(x)
+    new_df['entities'] = new_entities
+    return new_df
 
