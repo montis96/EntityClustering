dfe5df8f601c21131955b32ca8c640b9e82a62e5
Simone Monti <sgmonti@gmail.com>
2021-08-02 19:15:47 +0200

    wip resolving ambiguity

--- Packages/ClusteringHelper.py
+++ Packages/ClusteringHelper.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 import codecs
 from pathlib import Path
-from nltk import WhitespaceTokenizer
+from collections import Counter
 import numpy as np
 
 """
@@ -22,6 +22,8 @@ def read_aida_yago_conll(raw_path):
     numeric_codes = []
     alpha_codes = []
     counter = 0
+    word_counter = 0
+    word_indexes = []
     indexes = []
     documents = []
     document = ""
@@ -70,16 +72,23 @@ def read_aida_yago_conll(raw_path):
                 try:
                     if word[1] == "B":
                         indexes.append((counter, counter + len(tokens[-1])))
+                        word_indexes.append(word_counter)
                 except:
                     indexes.append((counter, counter + len(tokens[-1])))
+                    word_indexes.append(word_counter)
                 try:
                     if word[1] == "B":
                         counter = counter + len(tokens[-1]) + 1
+                        word_counter = word_counter + 1
+                    else:
+                        word_counter = word_counter + 1
                 except:
                     counter = counter + len(tokens[-1]) + 1
+                    word_counter = word_counter + 1
         else:
             document = int(word[0].split()[1][1:].replace("testa", "").replace("testb", "")) - 1
             counter = 0
+            word_counter = 0
 
     for i in range(len(tags)):
         if entities[i] == '--NME--':
@@ -87,8 +96,8 @@ def read_aida_yago_conll(raw_path):
             tags[i] = ''
             mentions[i] = ''
     dataframe = pd.DataFrame(
-        list(zip(documents, tokens, indexes, tags, mentions, entities, wikidatas, numeric_codes, alpha_codes)),
-        columns=["documents", "tokens", "indexes", "tags", "mentions", "entities", "wikidatas",
+        list(zip(documents, tokens, indexes, word_indexes, mentions, entities, wikidatas, numeric_codes, alpha_codes)),
+        columns=["documents", "tokens", "indexes", 'word_indexes', "mentions", "entities", "wikidatas",
                  "numeric_codes", "alpha_codes"])
     texts = text_reconstruction(dataframe)
     return texts, dataframe
@@ -101,9 +110,18 @@ def text_reconstruction(dataframe):
     return texts
 
 
+def get_gold_standard_dict(dataframe):
+    ents_data = dataframe[dataframe['entities'] != '']
+    golden_standard_entities = ents_data['entities'].values
+    golden_standard_dict = Counter(golden_standard_entities)
+    golden_standard_dict = dict(sorted(golden_standard_dict.items(), key=lambda item: item[1], reverse=True))
+    return golden_standard_dict
+
+
 def filter_data(dataframe, min_el):
     new_df = dataframe.copy()
     entities = new_df['entities']
+    mentions = new_df['mentions']
     not_allowed = []
     from collections import Counter
     ent_dict = Counter(entities)
@@ -111,11 +129,62 @@ def filter_data(dataframe, min_el):
         if ent_dict[key] < min_el:
             not_allowed.append(key)
     new_entities = []
-    for x in new_df['entities']:
-        if x in not_allowed:
+    new_mentions = []
+    for x in range(len(entities)):
+        if entities[x] in not_allowed:
             new_entities.append('')
+            new_mentions.append('')
         else:
-            new_entities.append(x)
+            new_entities.append(entities[x])
+            new_mentions.append(mentions[x])
     new_df['entities'] = new_entities
+    new_df['mentions'] = new_mentions
     return new_df
 
+
+def get_context(splitted_sentence, word_index, windows=128):
+    half_window = round(windows / 2, 0)
+    starting_index = word_index - half_window
+    ending_index = word_index + half_window
+    doc_len = len(splitted_sentence) - 1
+    if starting_index < 0:
+        ending_index = ending_index - starting_index
+        starting_index = 0
+        if ending_index > doc_len:
+            ending_index = doc_len
+    if ending_index > doc_len:
+        starting_index = starting_index - doc_len + ending_index
+        ending_index = doc_len
+        if starting_index < 0:
+            starting_index = 0
+    return int(starting_index), int(ending_index)
+
+
+def calculate_context_vector(model, cluster, text_splitted, doc_window=20, window=100, doc_context_alpha=0.4,
+                             word_context_aplha=0.6):
+    arr_length = len(model['word'])
+    contexts_vectorized = []
+    for el in cluster:
+        doc_context_mean = np.zeros(arr_length)
+        actual_doc_window = doc_window if doc_window < len(text_splitted[el[2]]) else len(text_splitted[el[2]])
+
+        null_counter = 0
+        for i in range(actual_doc_window):
+            try:
+                doc_context_mean += model[text_splitted[el[2]][i]]
+            except:
+                null_counter += 1
+        doc_context_mean = doc_context_mean / (actual_doc_window - null_counter)
+
+        null_counter = 0
+        word_context_mean = np.zeros(arr_length)
+        for word in text_splitted[el[2]][el[4][0]:el[4][1]]:
+            try:
+                word_context_mean += model[word]
+            except:
+                null_counter += 1
+
+        word_context_mean = word_context_mean / (window - null_counter)
+        final_mean = word_context_mean * word_context_aplha + doc_context_mean * doc_context_alpha
+        contexts_vectorized.append(final_mean)
+    return contexts_vectorized
--- clustering/Baseline.py
+++ clustering/Baseline.py
@@ -1,24 +1,35 @@
 #%%
 
-import Packages.CoNLLReader as reader
-import numpy as np
+import Packages.ClusteringHelper as ch
 from tqdm import tqdm
 from Levenshtein import distance
+from collections import Counter
 
 #%%
 
-text, data = reader.read_aida_yago_conll(
+text, data = ch.read_aida_yago_conll(
     "D:\\Sgmon\\Documents\\Magistrale\\TESI\\ClusteringAndLinking\\aida-yago2-dataset\\AIDA-YAGO2-dataset.tsv")
 save = False
 if save:
     text_file = open('text.txt', 'w')
     text_file.write(text)
     text_file.close()
+data
+#%%
+
+data = ch.filter_data(data, 3)
+
+#%% md
+
+### General Info
+
 #%%
 
 n_entities = sum([x is not '' for x in list(data['entities'])])
 n_ass_ents = sum([x is not '' for x in list(data['numeric_codes'])])
-n_tokens = sum([1 for x in list(data['entities'])])
+# n_tokens = sum([1 for x in list(data['entities'])])
+n_tokens = sum([len(x.split()) for x in text])
+
 
 #%%
 
@@ -26,34 +37,23 @@ print('{0:<35} {1:>10} '.format("Numero totale di entità:", n_entities))
 print('{0:<35} {1:>10} '.format("Numero totale di tokens:", n_tokens))
 print('{0:<35} {1:>10} '.format("1 entità ogni:", round(n_tokens / n_entities, 2)))
 
-#%%
-
-ent_nums = {}
-ents_data = data[data['entities'] != '']
-entities = ents_data['entities'].values
-unique_entities = np.unique(entities)
-for uniqu_ent in tqdm(entities):
-    ent_nums[uniqu_ent] = 0
-for uniqu_ent in tqdm(entities):
-    ent_nums[uniqu_ent] = ent_nums[uniqu_ent] + 1
-ent_nums = dict(sorted(ent_nums.items(), key=lambda item: item[1], reverse=True))
-#%%
-ents_data = data[data['entities'] != '']
-mentions = ents_data['mentions'].values
-
-mention_to_entity = {}
-for men, ent in set(list(zip(list(ents_data['mentions']), list(ents_data['entities'])))):
-    mention_to_entity[men] = ent
 #%% md
-### Golden standard
+
+### Gold standard
+
 #%%
-golden_standard = [mention_to_entity[x] for x in mentions]
-golden_standard_dict = dict.fromkeys(golden_standard, 0)
-for ent in golden_standard:
-    golden_standard_dict[ent] = golden_standard_dict[ent] + 1
+
+golden_standard_dict = ch.get_gold_standard_dict(data)
+#%%
+ents_data = data[data['entities'] != '']
+golden_standard_entities = ents_data['entities'].values
+mentions = ents_data['mentions'].values
+mentions = [x.lower() for x in mentions]
+
 #%% md
 
 ### Clustering by Levenshtein distance and DBSCAN
+
 #%%
 
 # Way with dbscan algorithm
@@ -74,76 +74,7 @@ if clustering:
 
 
     X = np.arange(len(mentions)).reshape(-1, 1)
-    _, leven_cluster = dbscan(X, metric=lev_metric, eps=1, min_samples=5, n_jobs=-1)
-    np.savetxt('db_cluster_levestein_5.txt', leven_cluster, delimiter=',')
+    _, leven_cluster = dbscan(X, metric=lev_metric, eps=1, min_samples=0, n_jobs=-1)
+    np.savetxt('db_cluster_levestein:0_3.txt', leven_cluster, delimiter=',')
 else:
-    leven_cluster = np.loadtxt("../aida-yago2-dataset/db_cluster_levestein_5.txt", dtype=np.int32)
-#%%
-# Now we create a dict for each cluster taht contains entities and entities count
-lev_cluster_dict = {}
-for i, x in enumerate(leven_cluster):
-    try:
-        lev_cluster_dict[x].append(mentions[i])
-    except:
-        lev_cluster_dict[x] = [mentions[i]]
-# del lev_cluster_dict[-1]
-for key in tqdm(lev_cluster_dict):
-    cluster_list = [mention_to_entity[x] for x in lev_cluster_dict[key]]
-    cluster_set = set(cluster_list)
-    cluster_dict = dict.fromkeys(list(cluster_set), 0)
-    for cluster in cluster_list:
-        cluster_dict[cluster] = cluster_dict[cluster] + 1
-    lev_cluster_dict[key] = cluster_dict
-#%% md
-
-### evaluation levestein
-#%%
-max_lev_cluster_list = []
-for key in lev_cluster_dict.keys():
-    max_key = max(lev_cluster_dict[key], key=lev_cluster_dict[key].get)
-    max_lev_cluster_list.append((max_key, lev_cluster_dict[key][max_key]))
-max_lev_cluster_list.sort(key=lambda x: x[1], reverse=True)
-max_lev_cluster_dict = dict.fromkeys(entities, 0)
-for key, val in max_lev_cluster_list:
-    if max_lev_cluster_dict[key] == 0:
-        max_lev_cluster_dict[key] = val
-#%%
-# CEAFm_levenshtein_precision
-CEAFm_levenshtein_precision = sum([x for x in max_lev_cluster_dict.values()]) / ents_data.shape[0]
-#%%
-# CEAFm_levenshtein_recall
-CEAFm_levenshtein_recall = sum([x for x in max_lev_cluster_dict.values()]) / sum(
-    [y for x in lev_cluster_dict.values() for y in x.values()])
-#%%
-# CEAFm_levenshtein_f1
-CEAFm_levenshtein_f1 = (2 * (CEAFm_levenshtein_recall * CEAFm_levenshtein_precision)) / (
-        CEAFm_levenshtein_precision + CEAFm_levenshtein_recall)
-#%%
-# B-cubed - recall
-bcubed_recall_num = 0
-for gold_key in tqdm(golden_standard_dict.keys()):
-    for lev_key in lev_cluster_dict.keys():
-        try:
-            bcubed_recall_num = bcubed_recall_num + (pow(lev_cluster_dict[lev_key][gold_key], 2) /
-                                       golden_standard_dict[gold_key])
-        except:
-            pass
-bcubed_recall = bcubed_recall_num/ents_data.shape[0]
-#%%
-# B-cubed - precision
-bcubed_precision_num = 0
-for gold_key in tqdm(golden_standard_dict.keys()):
-    for lev_key in lev_cluster_dict.keys():
-        try:
-            bcubed_precision_num = bcubed_precision_num + (pow(lev_cluster_dict[lev_key][gold_key], 2) /
-                                       sum([x for x in lev_cluster_dict[lev_key].values()]))
-        except:
-            pass
-bcubed_precision = bcubed_precision_num/sum([y for x in lev_cluster_dict.values() for y in x.values()])
-#%%
-bcubed_f1 = (2 * (bcubed_recall * bcubed_precision)) / (
-        bcubed_precision + bcubed_recall)
-#%%
-
-
-
+    leven_cluster = np.loadtxt("../aida-yago2-dataset/db_cluster_levestein_3_3.txt", dtype=np.int32)
