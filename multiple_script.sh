7b4580459ed52cfd3c0670d1fd0384795377c337
Simone Monti <sgmonti@gmail.com>
2021-10-07 16:31:44 +0200

    final 1 fix

--- multiple_script.sh
+++ multiple_script.sh
@@ -10,6 +10,6 @@ sleep 3
   python ./evolving_clustering/script_ubuntu.py --step=120 --first_threshold="0.03678974213038678" --second_threshold="0.0155" --randomly --seed="42" --entropy="10"; } &
 sleep 3
 { echo "Secondo blocco";
+  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.017" --randomly --seed="42" --entropy="10";
   python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.02" --randomly --seed="42" --entropy="10";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.02" --randomly --seed="42" --entropy="10";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.02" --randomly --seed="42" --entropy="10";} &
+  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.022" --randomly --seed="42" --entropy="10";} &
