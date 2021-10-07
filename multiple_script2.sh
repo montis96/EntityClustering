723172b451fe075d8ecad31df1f05798a9964565
Simone Monti <sgmonti@gmail.com>
2021-10-07 16:30:55 +0200

    final 1

--- multiple_script2.sh
+++
@@ -1,8 +0,0 @@
-{ echo "Secondo blocco";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.017" --randomly --seed="42" ;} &
-sleep 3
-{ echo "Secondo blocco";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.018" --randomly --seed="42"; } &
-sleep 3
-{ echo "Secondo blocco";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.019" --randomly --seed="42";} &
