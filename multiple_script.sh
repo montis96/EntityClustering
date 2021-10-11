64b81f0059b9c68e2f91124901f21fc76445e896
Simone Monti <sgmonti@gmail.com>
2021-10-11 22:22:37 +0200

    creato bash per baseline

--- multiple_script.sh
+++ multiple_script.sh
@@ -1,15 +1,10 @@
 { echo "Secondo blocco";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.02" --randomly --seed="3" --entropy="10";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.02" --randomly --seed="80" --entropy="10";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.02" --randomly --seed="16" --entropy="10";} &
+  python ./evolving_clustering/script_baseline.py --step=1 --first_threshold="0.03678974213038678" --second_threshold="0.0155" --randomly --seed="42"; } &
 sleep 3
 { echo "Secondo blocco";
-  python ./evolving_clustering/script_ubuntu.py --step=1 --first_threshold="0.03678974213038678" --second_threshold="0.0155" --randomly --seed="42" --entropy="10";
-  python ./evolving_clustering/script_ubuntu.py --step=30 --first_threshold="0.03678974213038678" --second_threshold="0.0155" --randomly --seed="42" --entropy="10";
-  python ./evolving_clustering/script_ubuntu.py --step=70 --first_threshold="0.03678974213038678" --second_threshold="0.0155" --randomly --seed="42" --entropy="10";
-  python ./evolving_clustering/script_ubuntu.py --step=120 --first_threshold="0.03678974213038678" --second_threshold="0.0155" --randomly --seed="42" --entropy="10"; } &
+  python ./evolving_clustering/script_baseline.py --step=70 --first_threshold="0.03678974213038678" --second_threshold="0.0155" --randomly --seed="42" ;
+  python ./evolving_clustering/script_baseline.py --step=120 --first_threshold="0.03678974213038678" --second_threshold="0.0155" --randomly --seed="42" ; } &
 sleep 3
 { echo "Secondo blocco";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.017" --randomly --seed="42" --entropy="10";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.02" --randomly --seed="42" --entropy="10";
-  python ./evolving_clustering/script_ubuntu.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.022" --randomly --seed="42" --entropy="10";} &
+  python ./evolving_clustering/script_baseline.py --step=10 --first_threshold="0.03678974213038678" --second_threshold="0.0155" --randomly --seed="42" ;
+  python ./evolving_clustering/script_baseline.py --step=30 --first_threshold="0.03678974213038678" --second_threshold="0.0155" --randomly --seed="42" ; } &
